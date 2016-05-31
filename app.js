059e99fc7dbb71844a4517a87926053100f264de
krpeacock <kylpeacock@gmail.com>
2016-05-31 16:54:24 -0700

    pug to jade

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require("path");
 const favicon = require('serve-favicon');
 
 
-app.set('view engine','pug');
+app.set('view engine','jade');
 app.use(express.static(__dirname + "/public"));
 app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 
